49a7bbbd38a60ea96fd2415db6a80a147003b22a
IvanRamirez01 <104411573+IvanRamirez01@users.noreply.github.com>
2023-02-08 18:46:33 -0600

    Add files via upload

---
+++ calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,21 @@
+
+package calculadora;
+
+public class Calculadora {
+    
+    public double resultado;
+    
+    public void calcsuma(double n1, double n2){
+        resultado = n1 + n2;
+    }
+    public void calcresta(double n1, double n2){
+        resultado = n1 - n2;
+    }
+    public void calcmult(double n1, double n2){
+        resultado = n1 * n2;
+    }
+    public void calcdiv(double n1, double n2){
+        resultado = n1 / n2;
+    }
+    
+}
